5fb828589b5ca0a7dec77a50a9e9627d50de30c3
Haon607 <noah0676@gmail.com>
2025-01-01 16:41:52 +0100

    scoreboard

---
+++ src/app/services/scoreboard.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Subject } from "rxjs";
+import { MemoryService } from "./memory.service";
+import { ScoreboardComponent } from "../components/scoreboard/scoreboard.component";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScoreboardService {
+  playerSubject = new Subject<[ScoreboardPlayer[], boolean]>();
+  sortSubject = new Subject<void>();
+
+
+  constructor(private memory: MemoryService) {
+    this.playerSubject.next([memory.players.map(player => {
+      return {
+        name: player.name,
+        score: player.gameScore,
+        pointAward: undefined,
+        square: undefined,
+        active: false
+      }
+    }), false]);
+    this.sortSubject.next()
+
+  }
+}
+export interface ScoreboardPlayer {
+  name: string;
+  score: number;
+  pointAward: number | undefined;
+  square: ScoreboardSquare | undefined;
+  active: boolean
+}
+export class ScoreboardSquare {
+  squareBackground?: string;
+  squareBorder?: string;
+  squareText?: string;
+}
